9fccd2083cc2dabd751807b9b452b7980c0f6b8a
Kazutaka Tsuji <kazutaka.tsuji@kazutak-ltm05cn.internal.salesforce.com>
2022-02-10 10:32:04 +0900

    fix error and design

--- InputDataUsingForms/force-app/main/default/aura/campingList/campingListController.js
+++ InputDataUsingForms/force-app/main/default/aura/campingList/campingListController.js
@@ -7,11 +7,14 @@
         }, true);
         if(validExpense) {
             let newCampingItem = component.get("v.newItem");
-            console.log("Create camping item: " + JSON.stringify(newCampingItem));
+            console.log("Before Create camping item: " + JSON.stringify(newCampingItem));
             let campingItem = component.get("v.items");
-            let item = JSON.parse(JSON.stringify(campingItem));
-            campingItem.push(item);
-            component.set("v.items", item);
+            console.log("campingItem:" + JSON.stringify(campingItem))
+            campingItem.push(newCampingItem);
+            console.log("After Create camping item: " + JSON.stringify(campingItem));
+            // set to items array
+            component.set("v.items", campingItem);
+            // initialize
             component.set("v.newItem", {"sobjectType": 'Camping_Item__c',
                           "Name": '', "Quantity__c": 0, "Price__c": 0, "Packed__c": false});
         }
